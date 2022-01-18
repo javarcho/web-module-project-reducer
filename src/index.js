c82338ed950e75f643c67aa57e785eacb64ea162
javarcho <alanvarcho@gmail.com>
2022-01-18 16:49:00 -0600

    mvp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,78 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './components/App'
-import './style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-render(
-  <App />,
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY, ADD_TO_MEMORY, ADD_MEMORY_TO_TOTAL, CLEAR_MEMORY } from './../actions/index';
+
+export const initialState = {
+    total: 0,
+    operation: "+",
+    memory: 0
+}
+
+const calculateResult = (num1, num2, operation) => {
+  switch(operation) {
+      case("+"):
+          return num1 + num2;
+      case("*"):
+          return num1 * num2;
+      case("-"):
+          return num1 - num2;
+  }
+}
+const reducer = (state, action) => {
+    switch(action.type) {
+        case(ADD_ONE):
+            return({
+                ...state,
+                total: state.total + 1
+            });
+        case(APPLY_NUMBER):
+            return ({ 
+                ...state, 
+                total: calculateResult(state.total, action.payload, state.operation)
+            });
+        
+        case(CHANGE_OPERATION):
+            return ({
+                ...state,
+                operation: action.payload
+            });
+        case(CLEAR_DISPLAY):
+            return ({
+            ...state,
+            total: 0
+        })
+        case(ADD_TO_MEMORY):
+            return ({
+            ...state,
+            memory: action.payload
+        })
+        case(ADD_MEMORY_TO_TOTAL):
+            return({
+                ...state,
+                total: calculateResult(state.total, action.payload, state.operation)
+            })
+        case(CLEAR_MEMORY):
+            return ({
+                ...state,
+                memory: 0
+            })
+
+        default:
+            return state;
+    }
+}
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
-)
+);
+
+reportWebVitals();
+export default reducer;
